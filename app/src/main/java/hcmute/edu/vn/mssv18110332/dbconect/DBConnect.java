85de1e0770cf5c31720977d87cbd76a8ab813490
ninhnq-github <57750928+ninhnq-github@users.noreply.github.com>
2021-07-27 13:07:00 +0700

    Update DBConnect.java

--- app/src/main/java/hcmute/edu/vn/mssv18110332/dbconect/DBConnect.java
+++ app/src/main/java/hcmute/edu/vn/mssv18110332/dbconect/DBConnect.java
@@ -13,12 +13,9 @@ public class DBConnect {
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            //String url = "jdbc:mysql://sql6.freemysqlhosting.net/sql6422522?useUnicode=true&characterEncoding=utf8";
             String url = "jdbc:mysql://192.168.20.107/onlineshoppingapp?useUnicode=true&characterEncoding=utf8";
-            //String user = "sql6422522";
-            //String pass = "Q8rWSQvq8E";
-            String user = "ninhnq";
-            String pass = "Nmaster2000";
+            String user = ""; // your username 
+            String pass = ""; //  your passowrd
             con = DriverManager.getConnection(url,user,pass);
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
